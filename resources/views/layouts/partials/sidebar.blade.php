dabcdf231a1ed18649c32e295c62550244a1af5d
Ahmed Sunil <iahmednill@gmail.com>
2026-05-05 17:47:35 +0500

    Moved Links and Icons into the App Management sidebar group, above Activity Log, and removed the separate CMS sidebar group.
      Changed: resources/views/layouts/partials/sidebar.blade.php:1

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -16,23 +16,6 @@
             ],
         ],
         [
-            'label' => 'CMS',
-            'items' => [
-                [
-                    'label' => 'Links',
-                    'route' => 'cms.links',
-                    'href'  => route('cms.links.index'),
-                    'icon'  => '<path stroke-linecap="round" stroke-linejoin="round" d="M13.19 8.688a4.5 4.5 0 0 1 1.242 7.244l-4.5 4.5a4.5 4.5 0 0 1-6.364-6.364l1.757-1.757m13.35-.622 1.757-1.757a4.5 4.5 0 0 0-6.364-6.364l-4.5 4.5a4.5 4.5 0 0 0 1.242 7.244"/>',
-                ],
-                [
-                    'label' => 'Icons',
-                    'route' => 'cms.icons',
-                    'href'  => route('cms.icons.index'),
-                    'icon'  => '<circle cx="12" cy="12" r="10"/><path stroke-linecap="round" stroke-linejoin="round" d="M8 12h8M12 8v8"/>',
-                ],
-            ],
-        ],
-        [
             'label' => 'User management',
             'items' => [
                 [
@@ -52,6 +35,18 @@
         [
             'label' => 'App Management',
             'items' => [
+                [
+                    'label' => 'Links',
+                    'route' => 'cms.links',
+                    'href'  => route('cms.links.index'),
+                    'icon'  => '<path stroke-linecap="round" stroke-linejoin="round" d="M13.19 8.688a4.5 4.5 0 0 1 1.242 7.244l-4.5 4.5a4.5 4.5 0 0 1-6.364-6.364l1.757-1.757m13.35-.622 1.757-1.757a4.5 4.5 0 0 0-6.364-6.364l-4.5 4.5a4.5 4.5 0 0 0 1.242 7.244"/>',
+                ],
+                [
+                    'label' => 'Icons',
+                    'route' => 'cms.icons',
+                    'href'  => route('cms.icons.index'),
+                    'icon'  => '<circle cx="12" cy="12" r="10"/><path stroke-linecap="round" stroke-linejoin="round" d="M8 12h8M12 8v8"/>',
+                ],
                 [
                     'label' => 'Activity Log',
                     'route' => 'app.activity',
